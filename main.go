5ba8dcf981d1d531f65416ed255d470f03008762
ryonakao <rodriguez.nak@gmail.com>
2017-10-21 15:44:46 +0900

    bigtable認証

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
+	"google.golang.org/api/option"
+
+	"cloud.google.com/go/bigtable"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+)
+
+const (
+	project  = "ca-intern-201710-team02"
+	instance = "teamb-bigtable1"
+)
+
+var (
+	ctx = context.Background()
+)
+
+func authenticate() *bigtable.Client {
+	pathToKeyFile := "ca-intern-201710-team02-4d5815ebcb43.json"
+	jsonKey, err := ioutil.ReadFile(pathToKeyFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	config, err := google.JWTConfigFromJSON(jsonKey, bigtable.Scope)
+	if err != nil {
+		fmt.Println(err)
+	}
+	client, err := bigtable.NewClient(ctx, project, instance, option.WithTokenSource(config.TokenSource(ctx)))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return client
+}
+
+func main() {
+	if os.Getenv("DEV") == "1" {
+		client := authenticate()
+		table := client.Open("latlon-table")
+		fmt.Println(table)
+	} else {
+		adminClient, err := bigtable.NewAdminClient(ctx, project, instance)
+		if err != nil {
+			log.Fatalln("エラー", err)
+		}
+		tables, err := adminClient.Tables(ctx)
+		if err != nil {
+			log.Fatalln("エラー", err)
+		}
+		log.Println(tables)
+	}
 }
